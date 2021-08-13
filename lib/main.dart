fa3702b4aa4d66222e35c2c1f85f37547bd3a574
Darshan Patel <darshanthumar2901@gmail.com>
2021-08-13 18:41:30 +0530

    01

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+// import 'dart:async';
 import 'package:flutter/material.dart';
+// import 'package:barcode_scan/barcode_scan.dart';
 
 void main() => runApp(new MaterialApp(
-      home: Dashboard(),
+      home: LoginPage(),
       theme: new ThemeData(
           primarySwatch: Colors.blue,
           brightness: Brightness.light,
@@ -98,7 +100,7 @@ class Dashboard extends StatelessWidget {
                                     color: Colors.black,
                                     fontWeight: FontWeight.bold,
                                     fontSize: 18.0),
-                              )
+                              ),
                             ],
                           ),
                         ),
@@ -223,6 +225,145 @@ class Dashboard extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    final buttonLogin = Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Container(
+        width: size.width * 0.6,
+        child: ButtonTheme(
+          height: 56,
+          child: RaisedButton(
+            child: Text('Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                )),
+            color: Colors.black87,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            onPressed: () => {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Dashboard())),
+            },
+          ),
+        ),
+      ),
+    );
+
+    final buttonForgotPassword = FlatButton(
+        child: Text(
+          'Forgot Password',
+          style: TextStyle(color: Colors.grey, fontSize: 16),
+        ),
+        onPressed: null);
+
+    final contactUs = SizedBox(
+      child: Text('Contact Help Center',
+          style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 20,
+              fontWeight: FontWeight.w900,
+              decoration: TextDecoration.underline)),
+    );
+
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        centerTitle: true,
+        title: new Text("Digital Memo"),
+      ),
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Image.asset('assets/newlogo.jpeg'),
+              new Form(
+                  child: new Theme(
+                      data: new ThemeData(
+                          primarySwatch: Colors.teal,
+                          inputDecorationTheme: new InputDecorationTheme(
+                              labelStyle: new TextStyle(
+                                  color: Colors.black87, fontSize: 60.0))),
+                      child: new Container(
+                        width: size.width * 1.2,
+                        padding: const EdgeInsets.all(20),
+                        child: new Column(
+                          children: <Widget>[
+                            new TextFormField(
+                              decoration: new InputDecoration(
+                                hintText: "Enter Email",
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            new TextFormField(
+                              decoration: new InputDecoration(
+                                hintText: "Enter Password",
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              obscureText: true,
+                            ),
+                            buttonLogin,
+                            buttonForgotPassword,
+                            contactUs
+                          ],
+                        ),
+                      )))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class QrPage extends StatefulWidget {
+  @override
+  _QrPageState createState() => _QrPageState();
+}
+
+class _QrPageState extends State<QrPage> {
+  String result = "Scan Here!";
+
+  // Future _scanQR() async{
+  //   try{
+  //     String qrResult = await BarcodeScanner.scan();
+  //     setState(() {
+  //       result = qrResult;
+  //     });
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          icon: Icon(Icons.camera_alt), onPressed: () {}, label: Text("Scan")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
